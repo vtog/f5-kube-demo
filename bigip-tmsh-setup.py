80eda6d6c70b0932b1adc4f3d1f6ff4f2fbd20a5
vtog <v.tognaci@f5.com>
2018-02-23 17:29:27 -0600

    bigip setup via REST

--- bigip-tmsh-setup.py
+++ bigip-tmsh-setup.py
@@ -5,7 +5,7 @@ import requests
 #Create partition
 url = "https://10.1.20.10/mgmt/tm/auth/partition"
 
-payload = "{\n\t\"name\": \"kubernetes\",\n    \"defaultRouteDomain\": 0\n\t\n}"
+payload = "{\n    \"name\": \"kubernetes\",\n    \"defaultRouteDomain\": 0,\n    \"description\": \"Repository for Kubernetes managed objects.\"\n}"
 headers = {
     'Content-Type': "application/json",
     'Authorization': "Basic YWRtaW46YWRtaW4=",
@@ -18,8 +18,7 @@ print(response.text)
 
 #Create vxlan profile
 url = "https://10.1.20.10/mgmt/tm/net/tunnels/vxlan"
-
-payload = "{\n    \"name\": \"fl-vxlan\",\n    \"partition\": \"Common\",\n    \"defaultsFrom\": \"/Common/vxlan\",\n    \"encapsulationType\": \"vxlan\",\n    \"floodingType\": \"none\",\n    \"port\": 8472\n}"
+payload = "{\n    \"name\": \"fl-vxlan\",\n    \"description\": \"Flannel vxlan profile for kubernetes cluster deployment\",\n    \"partition\": \"Common\",\n    \"defaultsFrom\": \"/Common/vxlan\",\n    \"encapsulationType\": \"vxlan\",\n    \"floodingType\": \"none\",\n    \"port\": 8472\n}"
 headers = {
     'Content-Type': "application/json",
     'Authorization': "Basic YWRtaW46YWRtaW4=",
@@ -45,11 +44,23 @@ response = requests.request("POST", url, data=payload, headers=headers, verify=F
 print(response.text)
 
 #Create Flannel self-IP
-import requests
-
 url = "https://10.1.20.10/mgmt/tm/net/self"
 
-payload = "{\n    \"name\": \"10.244.20.10\",\n    \"description\": \"IP address to communicate on the Flannel kubernetes network\",\n    \"partition\": \"Common\",\n    \"address\": \"10.244.20.10/16\",\n    \"floating\": \"disabled\",\n    \"inheritedTrafficGroup\": \"false\",\n    \"trafficGroup\": \"/Common/traffic-group-local-only\",\n    \"unit\": 0,\n    \"vlan\": \"/Common/fl-vxlan\"\n}"
+payload = "{\n    \"name\": \"10.244.20.10\",\n    \"description\": \"Self-IP address to communicate on the Flannel kubernetes network\",\n    \"partition\": \"Common\",\n    \"address\": \"10.244.20.10/16\",\n    \"floating\": \"disabled\",\n    \"inheritedTrafficGroup\": \"false\",\n    \"trafficGroup\": \"/Common/traffic-group-local-only\",\n    \"unit\": 0,\n    \"vlan\": \"/Common/fl-vxlan\",\n    \"allowService\": \"none\"\n}"
+headers = {
+    'Content-Type': "application/json",
+    'Authorization': "Basic YWRtaW46YWRtaW4=",
+    'Cache-Control': "no-cache",
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+
+print(response.text)
+
+#Create Flannel Float-IP
+url = "https://10.1.20.10/mgmt/tm/net/self"
+
+payload = "{\n    \"name\": \"10.244.20.11\",\n    \"description\": \"Float-IP address to communicate on the Flannel kubernetes network\",\n    \"partition\": \"Common\",\n    \"address\": \"10.244.20.11/16\",\n    \"floating\": \"enabled\",\n    \"inheritedTrafficGroup\": \"true\",\n    \"trafficGroup\": \"/Common/traffic-group-1\",\n    \"unit\": 0,\n    \"vlan\": \"/Common/fl-vxlan\",\n    \"allowService\": \"none\"\n}"
 headers = {
     'Content-Type': "application/json",
     'Authorization': "Basic YWRtaW46YWRtaW4=",
